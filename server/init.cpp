a6b3fd66d175d4a0ec2fcd4e40eac2208ddade7a
Kevin Zheng <kevinz5000@gmail.com>
2021-08-08 17:34:49 -0700

    Add weather test program

--- server/init.cpp
+++ server/init.cpp
@@ -37,7 +37,7 @@
 #include "modules.h"
 
 static void help(void);
-static void init_beforeplay(void);
+void init_beforeplay(void);
 static void init_startup(void);
 
 /** If set after command line argument parsing, then the server will exit. */
@@ -606,7 +606,7 @@ static void free_materials(void) {
  * be here or in the common directory - but since only the server needs this
  * information, having it here probably makes more sense.
  */
-static void load_settings(void) {
+void load_settings(void) {
     char buf[MAX_BUF], *cp, dummy[1];
     int has_val;
     FILE *fp;
---
+++ utils/test_weather.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "global.h"
+
+extern unsigned long todtick;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        puts("usage: test_weather NUM_TICKS");
+        return 1;
+    }
+
+    int num = atoi(argv[1]);
+
+    init_library();
+    load_settings(); // for worldmap coordinates
+    settings.dynamiclevel = 2; // overwrite settings
+    init_beforeplay();
+    init_weather();
+    LOG(llevInfo, "Simulating weather for %d ticks: ", num);
+    for (int i = 0; i < num; i++) {
+        timeofday_t tod;
+        todtick++;
+        get_tod(&tod);
+        tick_weather();
+        if (tod.hour == 0) {
+            process_rain();
+        }
+        write_weather_images();
+        char filename[MAX_BUF];
+        char filename2[MAX_BUF];
+        snprintf(filename, sizeof(filename), "%s/weather.ppm", settings.localdir);
+        snprintf(filename2, sizeof(filename2), "%s/weather.%d.ppm", settings.localdir, i);
+        rename(filename, filename2);
+        putchar('.');
+    }
+    putchar('\n');
+}
