6f7aaeb6b31e9fa00c22d2263e949a436fb5e07f
kettek <kts@kettek.net>
2025-03-04 03:25:54 -0800

    Adjust build output location to Release or Debug

--- make_win32/get_maps/clone.bat
+++ make_win32/get_maps/clone.bat
@@ -1,5 +1,6 @@
 echo off
 set dir=%1
 cd %dir%
-md -force share
-git clone https://git.code.sf.net/p/crossfire/crossfire-maps share/maps
+if not exist share mkdir share
+if exist share/maps cd share/maps & git pull
+if not exist share/maps git clone https://git.code.sf.net/p/crossfire/crossfire-maps share/maps
